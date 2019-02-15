5571c54ff3107fd4c38a5b2e3c4c4f0d180ed1c5
yongshuang <yongshuang@gdgyun.com>
2019-02-15 18:21:35 +0800

    first-commit

---
+++ components/public/header/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <div class="m-header"> -->
+  <!-- </div> -->
+  <el-row :gutter="15" class="m-header">
+    <el-col :span="6"><geo/></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from './geo'
+export default {
+    components:{
+        Geo
+    }
+};
+</script>
+
+<style scoped>
+</style>
